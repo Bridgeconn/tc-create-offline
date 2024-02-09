9cd301a8eb69316a2c612b250ca1582a17f905eb
Joel C Johnson <joelcjohnson123@gmail.com>
2024-02-09 17:21:43 +0530

    initial setup of electron app with react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+React
+const App = () => {
+    return ( 
+        <div>
+            <h1>tC create Offline app</h1>
+        </div>
+     );
+}
+ 
+export default App;
