cc6374073bad7280bb7689d7b4f8b79ded78e7ac
Joel C Johnson <joelcjohnson123@gmail.com>
2024-04-02 09:56:25 +0530

    solved issue when clicking open file button

--- src/js/App.js
+++ src/js/App.js
@@ -3,6 +3,7 @@ import Component from './Component'
 import InputAdornment from '@material-ui/core/InputAdornment';
 import FolderOpenIcon from '@material-ui/icons/FolderOpen';
 import MarkdownTranslatable from './MarkdownTranslatable';
+import MdOffline from './MdOffline';
 import PrintPreview from "./PrintPreview";
 import AlertDialog from './Dialog';
 import { Button, TextField } from '@mui/material';
@@ -24,6 +25,7 @@ export default function App() {
   const [filePath, setFilePath] = useState("");
   const [fileType, setFileType] = useState("");
   let resType
+
 
 
   const showInFolder = () => {
@@ -116,8 +118,64 @@ export default function App() {
   };
 
   const handleDialogConfirm = () => {
-    setIsOpenDialog(false)
     clearState()
+    dialog.showOpenDialog({
+      defaultPath: path.join(__dirname, 'src', 'assets', 'Resources'),
+    }).then(
+      result => {
+        setLoading(true)
+        let file = result.filePaths[0].split(".")[0];
+
+        if (result.filePaths[0].includes("en_tn")) {
+          resType = "en_tn"
+          setResource("en_tn")
+        } else if (result.filePaths[0].includes("en_tq")) {
+          resType = "en_tq"
+          setResource("en_tq")
+
+        } else if (result.filePaths[0].includes("en_obs")) {
+          resType = "en_obs"
+          setResource("en_obs")
+        }
+
+        setFileName(file.substring(file.length - 3, file.length));
+        if (!result.canceled) {
+          let sourceFilePath = result.filePaths[0]
+          let fType = result.filePaths[0].split('.');
+          setFileType(fType[1])
+
+          document.getElementById("actual-file").value = sourceFilePath.split("Resources/")[1];
+
+          let fileInOutputName
+          let targetFilePath
+          if (resType === 'en_tn') {
+            fileInOutputName = result.filePaths[0].replace(`Resources/${resType}-release_v77`, "Output")
+            targetFilePath = (fs.existsSync(fileInOutputName) ? fileInOutputName : result.filePaths[0]);
+            document.getElementById("target-file").value = targetFilePath.includes("Resources") ? targetFilePath.split("Resources/")[1] : targetFilePath.split("assets/")[1];
+          } else if (resType === 'en_obs') {
+            fileInOutputName = result.filePaths[0].replace(`Resources/${resType}-v9`, "Output")
+            targetFilePath = (fs.existsSync(fileInOutputName) ? fileInOutputName : result.filePaths[0]);
+            document.getElementById("target-file").value = targetFilePath.includes("Resources") ? targetFilePath.split("Resources/")[1] : targetFilePath.split("assets/")[1];
+          }
+          let data = fs.readFileSync(sourceFilePath,
+            { encoding: 'utf8', flag: 'r' })
+          setSourceData(data);
+          if (sourceFilePath === targetFilePath) {
+            setTargetData(data)
+          }
+          else if ((sourceFilePath !== targetFilePath)) {
+            let targetdata = fs.readFileSync(targetFilePath,
+              { encoding: 'utf8', flag: 'r' })
+            setTargetData(targetdata)
+          }
+          setFilePath(targetFilePath)
+          setLoading(false);
+        }
+      }).catch(err => {
+        console.log(err)
+      })
+    setIsOpenDialog(false)
+
   };
 
   // console.log({ fileName, sourceData, targetData, resource, loading, filePath, fileType, editedContent })
@@ -179,11 +237,12 @@ export default function App() {
         <Grid xs={12} md={12} style={{ border: '1px black solid', margin: '15px' }}>
           {(sourceData?.length > 0 && targetData?.length > 0 && loading === false) ?
             (
-
               fileType === 'tsv' ?
                 <Component sourceData={sourceData} targetData={targetData} fileName={fileName} resource={resource} filePath={filePath} fileType={fileType} />
                 :
-                <MarkdownTranslatable sourceData={sourceData} targetData={targetData} fileName={fileName} resource={resource} filePath={filePath} fileType={fileType} />)
+                // <MdOffline sourceData={sourceData} targetData={targetData} fileName={fileName} resource={resource} filePath={filePath} fileType={fileType} />
+                <MarkdownTranslatable sourceData={sourceData} targetData={targetData} fileName={fileName} resource={resource} filePath={filePath} fileType={fileType} />
+            )
             : "Please select a file"}
 
         </Grid>
